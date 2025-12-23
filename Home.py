3672f67f4a8aaedbc2d65565ab147267ad42477b
unknown <alanwhassan@hotmail.com>
2025-12-22 21:40:20 -0300

    mensagem

--- Home.py
+++ Home.py
@@ -1,12 +1,36 @@
 import streamlit as st
 
+# --- Configuração da Página ---
 st.set_page_config(
     page_title="Início - Dashboard CBF",
     layout="wide"
 )
 
+# --- CSS para Fundo Transparente (Glassmorphism) ---
+page_bg_img = """
+<style>
+/* 1. Imagem de fundo no app */
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://img.freepik.com/vetores-gratis/fundo-abstrato-realista-de-futebol_52683-67579.jpg?semt=ais_hybrid&w=740&q=80");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+}
+
+/* 2. Remove cores de fundo padrão */
+[data-testid="stHeader"], [data-testid="stMainBlockContainer"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# --- Conteúdo da Página ---
 st.title("⚽ Dashboard de Análise do Campeonato")
 st.markdown("---")
+
 st.header("Seja Bem-vindo!")
 st.info("Utilize o menu de navegação na **barra lateral** à esquerda para acessar as diferentes seções de análise do campeonato.", icon="⬅️")
 
@@ -17,6 +41,5 @@ st.markdown("""
 * **⚔️ Duelo Times:** Indicadores dos times selecionados e resultados do 1º e 2º Turno entre os mesmos. 
 """)
 
-
 # --- Rodapé ---
-st.markdown("<br><hr><p style='text-align: center; color: gray;'>Dashboard de Análise de Performance | Autoria de Alan W. Hassan</p>", unsafe_allow_html=True)
+st.markdown("<br><hr><p style='text-align: center; color: #1E1E1E; font-weight: bold;'>Dashboard de Análise de Performance | Autoria de Alan W. Hassan</p>", unsafe_allow_html=True)
